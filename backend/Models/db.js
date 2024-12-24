d473990a03775add872e25b034f020a614559234
Dhairya <118837648+Dhairya0707@users.noreply.github.com>
2024-12-24 19:20:34 +0530

    change online cluster

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const lnk = process.env.MONGO_URL;
-const connection = mongoose.connect(lnk).then(() => {
-  console.log("connection done buddy !!");
-});
+const connection = mongoose
+  .connect(
+    "mongodb://dhairyadarji025:<wbGOBKAgKTnzliU8>@<hostname>/?ssl=true&replicaSet=atlas-aoldge-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connection done buddy !!");
+  });
 
 module.exports = connection;
