e99059ea7963179de7d49fc5194fa10f902169c8
Dhairya <118837648+Dhairya0707@users.noreply.github.com>
2024-12-24 19:31:32 +0530

    change url

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const lnk = process.env.MONGO_URL;
 const connection = mongoose
   .connect(
-    "mongodb://dhairyadarji025:<wbGOBKAgKTnzliU8>@<hostname>/?ssl=true&replicaSet=atlas-aoldge-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb://dhairyadarji025:wbGOBKAgKTnzliU8@cluster0-shard-00-00.abcde.mongodb.net:27017,cluster0-shard-00-01.abcde.mongodb.net:27017,cluster0-shard-00-02.abcde.mongodb.net:27017/?ssl=true&replicaSet=atlas-aoldge-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("connection done buddy !!");
